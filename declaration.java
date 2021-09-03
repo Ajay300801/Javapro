f48e2f28bd56bc402ef2e4abb0fcc4e392bbff4e
Ajay300801 <89722866+Ajay300801@users.noreply.github.com>
2021-09-03 23:57:12 +0530

    Add files via upload

---
+++ declaration.java
@@ -0,0 +1,15 @@
+package java;
+
+public class declaration{
+
+    public static void main(String[] args){
+  
+          boolean a = false;
+          byte b = 10;
+          int c = 34;
+          float d = 67.2f;
+          double e =12.3;
+          char f ='A';
+          System.out.printIn(a+""+b+""+c+""+d+""+e+""+f);
+     }
+}
